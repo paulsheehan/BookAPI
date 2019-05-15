c62da3f86b2411cef2530ba771dcdc504c435633
paulsheehan <p.sheehan94@gmail.com>
2019-05-15 17:33:36 +0100

    Added PUT to replace book information

---
+++ Routes/bookRoutes.js
@@ -0,0 +1,59 @@
+var express = require('express');
+
+var routes = function(Book) {
+    var bookRouter = express.Router();
+
+    bookRouter.route('/')
+    .post(function(req, res) {
+        var book = new Book(req.body);
+        book.save();
+        res.status(201).send(book);
+    })
+    .get(function(req, res) {
+
+        var query = {};
+        if(req.query.genre) {
+            query.genre = req.query.genre;
+        }
+        Book.find(query, function(err, books) {
+            if(err) {
+                res.status(500).send(err);
+            }
+            else {
+                res.json(books);
+            }
+        });
+    });
+
+    bookRouter.use('/:bookId', function(req, res, next) {
+        Book.findById(req.params.bookId, function(err, book) {
+            if(err) {
+                res.status(500).send(err);
+            }
+            else if(book) {
+                req.book = book;
+                next();
+            }
+            else {
+                res.status(404).send('no book found');
+            }
+        });
+    });
+
+    bookRouter.route('/:bookId')
+    .get(function(req, res) {
+        res.json(req.book);
+    })
+    .put(function(req, res) {
+        req.book.title = req.body.title;
+        req.book.author = req.body.author;
+        req.book.genre = req.body.genre;
+        req.book.read = req.body.read;
+        req.book.save();
+        res.json(book);
+    });
+
+    return bookRouter;
+};
+
+module.exports = routes;
--- app.js
+++ app.js
@@ -11,48 +11,12 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-
-var bookRouter = express.Router();
+var bookRouter = require('./Routes/bookRoutes')(Book);
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-bookRouter.route('/Books')
-    .post(function(req, res) {
-        var book = new Book(req.body);
-        book.save();
-        res.status(201).send(book);
-    })
-    .get(function(req, res) {
-
-        var query = {};
-        if(req.query.genre) {
-            query.genre = req.query.genre;
-        }
-        Book.find(query, function(err, books) {
-            if(err) {
-                res.status(500).send(err);
-            }
-            else {
-                res.json(books);
-            }
-        });
-    });
-
-
-bookRouter.route('/Books/:bookId')
-    .get(function(req, res) {
-        Book.findById(req.params.bookId, function(err, book) {
-            if(err) {
-                res.status(500).send(err);
-            }
-            else {
-                res.json(book);
-            }
-        });
-    });
-
-app.use('/api', bookRouter);
+app.use('/api/books', bookRouter);
 
 app.get('/', function(req, res) {
     res.send('Welcome to my API!');
