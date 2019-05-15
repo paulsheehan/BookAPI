62244670289e4fb652d89a3a1c5c6b545a9f327a
paulsheehan <p.sheehan94@gmail.com>
2019-05-15 09:46:47 +0100

    Added GET for /Books endpoint

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+    .get(function(req, res) {
+        var responseJson = {hello: 'This is my API'};
+
+        res.json(responseJson);
+    });
+
+app.use('/api', bookRouter);
+
 app.get('/', function(req, res) {
     res.send('Welcome to my API!');
 });
@@ -11,3 +23,4 @@ app.get('/', function(req, res) {
 app.listen(port, function() {
     console.log('Running on PORT: ' + port);
 });
+
